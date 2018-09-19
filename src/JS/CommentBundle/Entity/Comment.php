408d16e907c0e47c3196b1b1bf0968071bede7a1
Julien <schmittounet@gmail.com>
2018-09-19 10:41:56 +0200

    Use ACL to restrict comment to authenticated's users

--- src/JS/CommentBundle/Entity/Comment.php
+++ src/JS/CommentBundle/Entity/Comment.php
@@ -4,6 +4,9 @@ namespace JS\CommentBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 use FOS\CommentBundle\Entity\Comment as BaseComment;
+use FOS\CommentBundle\Model\SignedCommentInterface;
+use JS\UserBundle\Entity\User;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * Comment
@@ -11,7 +14,7 @@ use FOS\CommentBundle\Entity\Comment as BaseComment;
  * @ORM\Entity
  * @ORM\ChangeTrackingPolicy("DEFERRED_EXPLICIT")
  */
-class Comment extends BaseComment
+class Comment extends BaseComment implements SignedCommentInterface
 {
 
     /**
@@ -28,4 +31,41 @@ class Comment extends BaseComment
      * @ORM\ManyToOne(targetEntity="JS\CommentBundle\Entity\Thread")
      */
     protected $thread;
+
+    /**
+     * Author of the comment
+     *
+     * @ORM\ManyToOne(targetEntity="JS\UserBundle\Entity\User")
+     * @var User
+     */
+    protected $author;
+
+    /**
+     * Sets the author of the Comment.
+     *
+     * @param UserInterface $author
+     */
+    public function setAuthor(UserInterface $author)
+    {
+        $this->author = $author;
+    }
+
+    /**
+     * Gets the author of the Comment.
+     *
+     * @return UserInterface
+     */
+    public function getAuthor()
+    {
+        return $this->author;
+    }
+
+    public function getAuthorName()
+    {
+        if (null === $this->getAuthor()) {
+            return 'Anonymous';
+        }
+
+        return $this->getAuthor()->getUsername();
+    }
 }
